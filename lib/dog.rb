fc40850b1245b7ccc576da7b9f153ae61bd8f24d
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 18:38:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog 
   @@all = []
   
+  def initialize(name)
+  end 
+  
   def self.clear_all
     @@all = []
   end 
   
+  def self.all
+    @@all.map do |dog|
+      puts "#{dog}"
+    end 
+  end 
   
 end 
